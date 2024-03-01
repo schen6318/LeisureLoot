230e41c111588dd8bdd634ffbc624e072b6d334f
schen6318@conestogac.on.ca <schen6318@conestogac.on.ca>
2024-03-01 17:22:31 -0500

    improve getMap

--- client/src/Components/GetMap.js
+++ client/src/Components/GetMap.js
@@ -13,27 +13,25 @@ const GetMap = ({ center }) => {
   useEffect(() => {
     const initMap = async () => {
       if (window.google && center) {
-        // Ensure center is defined
-        // Request needed libraries
         const { Map } = await window.google.maps.importLibrary("maps");
         const { AdvancedMarkerElement } =
           await window.google.maps.importLibrary("marker");
 
         const map = new Map(mapRef.current, {
-          center, // Use the center prop
+          center,
           zoom: 14,
           mapId: mapId,
         });
 
         new AdvancedMarkerElement({
           map: map,
-          position: center, // Use the center prop
+          position: center,
         });
       }
     };
 
     initMap();
-  }, [center]); // Re-run when center changes
+  }, [center]);
 
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
