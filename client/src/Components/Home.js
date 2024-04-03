263057370b597352ee88db3aa9a368b1540216d9
schen6318@conestogac.on.ca <schen6318@conestogac.on.ca>
2024-04-03 12:06:02 -0400

    修复home page modal显示错误

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ContactForm from "./ContactForm";
-const profileImg = "/assets/img/profile.png";
 const cabinImg = "/assets/img/portfolio/cabin.png";
 const cakeImg = "/assets/img/portfolio/cake.png";
 const circusImg = "/assets/img/portfolio/circus.png";
@@ -20,7 +19,7 @@ function Home() {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    pauseOnHover: true
+    pauseOnHover: true,
   };
 
   return (
@@ -115,7 +114,9 @@ function Home() {
               </div>
             </Slider>
           </div>
-          <h1 className="mt-5" style={{ "--bsBodyFontSize": "1rem"}}>UNLOCK YOUR POTENTIAL</h1>
+          <h1 className="mt-5" style={{ "--bsBodyFontSize": "1rem" }}>
+            UNLOCK YOUR POTENTIAL
+          </h1>
           <hr className="star-light" />
           <h2 className="font-weight-light mb-0">
             Share Your Skills, Earn Extra Income!
@@ -129,7 +130,51 @@ function Home() {
         tabIndex={-1}
         id="portfolio-modal-1"
       >
-        {/* Modal content */}
+        <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                className="btn-close"
+                type="button"
+                aria-label="Close"
+                data-bs-dismiss="modal"
+              />
+            </div>
+            <div className="modal-body">
+              <div className="container text-center">
+                <div className="row">
+                  <div className="col-lg-8 mx-auto">
+                    <h2 className="text-uppercase text-secondary mb-0">
+                      Woodworking
+                    </h2>
+                    <hr className="star-dark mb-5" />
+                    <img
+                      className="img-fluid mb-5"
+                      src="assets/img/portfolio/cabin.png"
+                      alt="cabin"
+                    />
+                    <p className="mb-5">
+                      Embrace the art of creation with wood. Whether you're a
+                      seasoned carpenter or a weekend warrior, showcase your
+                      woodworking skills, from crafting bespoke furniture to
+                      carving intricate details.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer pb-5">
+              <a
+                className="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss"
+                role="button"
+                data-bs-dismiss="modal"
+              >
+                <i className="fa fa-close" />
+                &nbsp;Close
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       <div
         className="modal text-center"
@@ -137,7 +182,50 @@ function Home() {
         tabIndex={-1}
         id="portfolio-modal-2"
       >
-        {/* Modal content */}
+        <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                className="btn-close"
+                type="button"
+                aria-label="Close"
+                data-bs-dismiss="modal"
+              />
+            </div>
+            <div className="modal-body">
+              <div className="container text-center">
+                <div className="row">
+                  <div className="col-lg-8 mx-auto">
+                    <h2 className="text-uppercase text-secondary mb-0">
+                      Baking
+                    </h2>
+                    <hr className="star-dark mb-5" />
+                    <img
+                      className="img-fluid mb-5"
+                      src="assets/img/portfolio/cake.png"
+                      alt="cake"
+                    />
+                    <p className="mb-5">
+                      Delight with sweet and savory bakes. Share your passion
+                      for baking, whether it's whipping up the perfect sourdough
+                      or decorating elaborate cakes for special occasions.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer pb-5">
+              <a
+                className="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss"
+                role="button"
+                data-bs-dismiss="modal"
+              >
+                <i className="fa fa-close" />
+                &nbsp;Close
+              </a>
+            </div>
+          </div>
+        </div>{" "}
       </div>
       <div
         className="modal text-center"
@@ -145,7 +233,51 @@ function Home() {
         tabIndex={-1}
         id="portfolio-modal-3"
       >
-        {/* Modal content */}
+        <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                className="btn-close"
+                type="button"
+                aria-label="Close"
+                data-bs-dismiss="modal"
+              />
+            </div>
+            <div className="modal-body">
+              <div className="container text-center">
+                <div className="row">
+                  <div className="col-lg-8 mx-auto">
+                    <h2 className="text-uppercase text-secondary mb-0">
+                      Programming
+                    </h2>
+                    <hr className="star-dark mb-5" />
+                    <img
+                      className="img-fluid mb-5"
+                      src="assets/img/portfolio/circus.png"
+                      alt="circus"
+                    />
+                    <p className="mb-5">
+                      Solve problems with code. Apply your analytical prowess to
+                      build apps, websites, and software solutions. Bring your
+                      expertise in various programming languages and frameworks
+                      to the table.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer pb-5">
+              <a
+                className="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss"
+                role="button"
+                data-bs-dismiss="modal"
+              >
+                <i className="fa fa-close" />
+                &nbsp;Close
+              </a>
+            </div>
+          </div>
+        </div>{" "}
       </div>
       <div
         className="modal text-center"
@@ -153,7 +285,50 @@ function Home() {
         tabIndex={-1}
         id="portfolio-modal-4"
       >
-        {/* Modal content */}
+        <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                className="btn-close"
+                type="button"
+                aria-label="Close"
+                data-bs-dismiss="modal"
+              />
+            </div>
+            <div className="modal-body">
+              <div className="container text-center">
+                <div className="row">
+                  <div className="col-lg-8 mx-auto">
+                    <h2 className="text-uppercase text-secondary mb-0">
+                      Graphic Design
+                    </h2>
+                    <hr className="star-dark mb-5" />
+                    <img
+                      className="img-fluid mb-5"
+                      src="assets/img/portfolio/game.png"
+                      alt="game"
+                    />
+                    <p className="mb-5">
+                      Visualize ideas with design. Use your creative flair to
+                      design stunning graphics, logos, and branding materials
+                      that communicate and captivate.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer pb-5">
+              <a
+                className="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss"
+                role="button"
+                data-bs-dismiss="modal"
+              >
+                <i className="fa fa-close" />
+                &nbsp;Close
+              </a>
+            </div>
+          </div>
+        </div>{" "}
       </div>
       <div
         className="modal text-center"
@@ -161,7 +336,50 @@ function Home() {
         tabIndex={-1}
         id="portfolio-modal-5"
       >
-        {/* Modal content */}
+        <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                className="btn-close"
+                type="button"
+                aria-label="Close"
+                data-bs-dismiss="modal"
+              />
+            </div>
+            <div className="modal-body">
+              <div className="container text-center">
+                <div className="row">
+                  <div className="col-lg-8 mx-auto">
+                    <h2 className="text-uppercase text-secondary mb-0">
+                      Gardening
+                    </h2>
+                    <hr className="star-dark mb-5" />
+                    <img
+                      className="img-fluid mb-5"
+                      src="assets/img/portfolio/safe.png"
+                      alt="safe"
+                    />
+                    <p className="mb-5">
+                      Cultivate growth and tranquility. Offer your green thumb
+                      services to nurture gardens, from planting seasonal blooms
+                      to maintaining lush landscapes.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer pb-5">
+              <a
+                className="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss"
+                role="button"
+                data-bs-dismiss="modal"
+              >
+                <i className="fa fa-close" />
+                &nbsp;Close
+              </a>
+            </div>
+          </div>
+        </div>{" "}
       </div>
       <div
         className="modal text-center"
@@ -169,7 +387,50 @@ function Home() {
         tabIndex={-1}
         id="portfolio-modal-6"
       >
-        {/* Modal content */}
+        <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                className="btn-close"
+                type="button"
+                aria-label="Close"
+                data-bs-dismiss="modal"
+              />
+            </div>
+            <div className="modal-body">
+              <div className="container text-center">
+                <div className="row">
+                  <div className="col-lg-8 mx-auto">
+                    <h2 className="text-uppercase text-secondary mb-0">
+                      Home Repair
+                    </h2>
+                    <hr className="star-dark mb-5" />
+                    <img
+                      className="img-fluid mb-5"
+                      src="assets/img/portfolio/submarine.png"
+                      alt="submarine"
+                    />
+                    <p className="mb-5">
+                      Fix, repair, and restore. With a toolkit and know-how,
+                      help others in the community by mending what's broken and
+                      improving homes one project at a time.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer pb-5">
+              <a
+                className="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss"
+                role="button"
+                data-bs-dismiss="modal"
+              >
+                <i className="fa fa-close" />
+                &nbsp;Close
+              </a>
+            </div>
+          </div>
+        </div>{" "}
       </div>
 
       <ContactForm />
